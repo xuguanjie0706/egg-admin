6d17e7cc04acd18e3b77e8ed522853345ad0e31b
xuguanjie <xuguanjie@halove.com>
2020-10-07 16:24:46 +0800

    bg xiugai

--- untils/index.js
+++ untils/index.js
@@ -8,7 +8,7 @@
  * @example getPasswords(6)
  */
 const getPasswords = (pasLen = 6) => {
-  const pasArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "-", "$", "%", "&", "@", "+", "!"];
+  const pasArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "-", "$", "%", "&", "@", "+", "!"];
   let password = "";
   const pasArrLen = pasArr.length;
   for (let i = 0; i < pasLen; i++) {
